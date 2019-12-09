1f158a1cf9000b4e8de4d1f938391a6d03cab6a6
Ben Letcher <bletcher@usgs.gov>
2019-12-09 17:08:37 -0500

    read in data works

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app>
+    <v-container fluid>
+      <v-row align="center">
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            :items="dataFileNames"
+            v-model="selectedFileName"
+            label="Data files"
+            solo
+          ></v-select>
 <!--    <timeseries-chart :extent="brushExtent" :data="filledData" v-if="filledData"></timeseries-chart>
     <brush-chart :extent="brushExtent" @brushed="onBrush"></brush-chart>
     <chunk-chart></chunk-chart> -->
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -19,113 +31,112 @@ export default {
 
   components: {
   },
-
   data: () => ({
     rawData: [],
     brushExtent: null,
-    movingMeanWindow: 10
+    movingMeanWindow: 10,
+    dataFileNames: ['1949884.csv', 'mitchellFromSHEDS.csv'],
+    selectedFileName: null
   }),
   computed: {
     filledData () {
-      console.log("filledData, start")
-      // fill data
+      console.log('filledData: start')
 
-   //   let dateRange = []
       let combined = []
-   //   let startIndex = 0
-   //   let dataFromRawData = null
+      let startIndex = 0
 
       // get date range for all data. Will fill in missing observations
       const rawDataFullRange = d3.timeDay.range(d3.min(this.rawData, d => d.date), d3.max(this.rawData, d => d.date))
 
-      // get all dates in rawData
-      const rawDataDates = this.rawData.map(d => formatters.mdy(d.date))
-//console.log(rawDataFullRange, rawDataDates)
+      // get all dates in dataByDay
+      const dataByDayDates = this.dataByDay.map(d => d.key)
+
+      console.log('rawDataDates', rawDataFullRange, dataByDayDates)
+
       // Loop over all dates and fill missing dates
       for (let i = 0; i < rawDataFullRange.length; i++) {
-        //console.log ('first',i)
-        //dateRange.push({ date: dateRange1[i], dayOfYear: +formatters.julian(dateRange1[i]) })
-        let dateIncludedTF = rawDataDates.includes(formatters.mdy(rawDataFullRange[i]))
-        //console.log ('second',i, dateIncludedTF)
+        let dateIncludedTF = dataByDayDates.includes(formatters.mdy(rawDataFullRange[i]))
+        if (i % 1000 === 0) console.log('second', i, dateIncludedTF, this.rawData.length, startIndex)
+
         if (!dateIncludedTF) { // missing data
-          console.log (i, rawDataFullRange[i], formatters.julian(rawDataFullRange[i]), this.dataByDayOfYear, +this.dataByDayOfYear[0].key)
-          let dataFromDataByDayOfYear = this.dataByDayOfYear.find(d => d.key === formatters.julian(rawDataFullRange[i])) // not so slow because only searching through 365 lines
-          console.log(dataFromDataByDayOfYear)
-
-          combined.push({ date: d.date, dayOfYear: d.dayOfYear, value: dataFromDataByDayOfYear.movingMean, missingData: true})     
-       
-
+          console.log('in !')
+          let dataFromDataByDayOfYear = this.dataByDayOfYear.find(d => +d.key === +formatters.julian(rawDataFullRange[i])) // not so slow because only searching through 365 lines
+          combined.push({
+            date: rawDataFullRange[i],
+            dayOfYear: +formatters.julian(rawDataFullRange[i]),
+            value: dataFromDataByDayOfYear.movingMean,
+            missingData: true })
+        } else { // existing data in rawData
+          for (let j = startIndex; j < dataByDayDates.length; j++) {
+            // console.log(i,j,formatters.mdy(rawDataFullRange[i]), dataByDayDates[j])
+            if (formatters.mdy(rawDataFullRange[i]) === dataByDayDates[j]) {
+              startIndex++
+              combined.push({
+                date: rawDataFullRange[i],
+                dayOfYear: +formatters.julian(rawDataFullRange[i]),
+                value: dataByDayDates[j].value,
+                missingData: false
+              })
+              break
+            }
+          }
         }
-
-
-
       }
-/*
-
-
-      for (let i = 0; i < dateRange.length; i++)
-        let dataFromDataInTF = rawDataDates.includes(formatters.mdy(d.date))
-
-
-        if (!dataFromDataInTF) { // missing data
-          console.log (d,i,dataFromDataInTF)
-          let dataFromDataByDayOfYear = this.dataByDayOfYear.find(dd => +dd.key === d.dayOfYear) // not so slow because only searching through 365 lines
-          combined.push({ date: d.date, dayOfYear: d.dayOfYear, value: dataFromDataByDayOfYear.movingMean, missingData: true })
-        } else {
-          // let dataFromRawData = findDate(this.startIndex, formatters.mdy(d.date))
-       //   for (let j = startIndex; j < this.rawData.length; j++) { 
-        //    console.log(i,j)
-        //    let dataInData = this.rawData[j]
-        //    if (formatters.mdy(d.date) === formatters.mdy(this.rawData[j].date)) {
-        //      startIndex++
-        //      dataFromRawData = dataInData
-        //    }
-        //  }
-     //     combined.push({ date: d.date, dayOfYear: d.dayOfYear, value: dataFromRawData.value, missingData: false })
-        } 
-      })
-*/
-console.log("filledData, end")
+      console.log('filledData: end', combined)
       return combined
     },
     chunkData () {
       // filledData + brushExtent -> chunked data
       // could be module later
+      return null
+    },
+    dataByDay () {
+      console.log('dataByDay:start')
+      let meanByDay = d3.nest()
+        .key(d => d.day)
+        .sortKeys((a, b) => d3.ascending(+a, +b))
+        .rollup(d => d3.mean(d, v => v.value))
+        .entries(this.rawData)
+
+      return meanByDay
     },
     dataByDayOfYear () {
-      console.log("dataByDayYear, start")
-      let meanByDay = d3.nest()
+      console.log('dataByDayOfYear:start')
+      let meanByDayOfYear = d3.nest()
         .key(d => +formatters.julian(d.date))
         .sortKeys((a, b) => d3.ascending(+a, +b))
         .rollup(d => d3.mean(d, v => v.value))
         .entries(this.rawData)
 
-      // get moving mean and add to meanByDay
+      // get moving mean and add to meanByDayOfYear
       // 'key' is julian day
-      const movingMean = [] 
-      for (var i = 0; i < meanByDay.length; i++) {
-        if (i < this.movingMeanWindow || (i > meanByDay.length - this.movingMeanWindow - 1)) {
-          meanByDay[i].movingMean = meanByDay[i].value
+      const movingMean = []
+      for (var i = 0; i < meanByDayOfYear.length; i++) {
+        if (i < this.movingMeanWindow || (i > meanByDayOfYear.length - this.movingMeanWindow - 1)) {
+          meanByDayOfYear[i].movingMean = meanByDayOfYear[i].value
         } else {
-          var total = 0;
-          for (var j = i - this.movingMeanWindow; j < i + this.movingMeanWindow;  j++) {
-            total += meanByDay[j].value
+          var total = 0
+          for (var j = i - this.movingMeanWindow; j < i + this.movingMeanWindow; j++) {
+            total += meanByDayOfYear[j].value
           }
           movingMean[i] = total / (this.movingMeanWindow * 2)
-          meanByDay[i].movingMean = movingMean[i]
-        } 
+          meanByDayOfYear[i].movingMean = movingMean[i]
+        }
       }
-      console.log("dataByDayYear, end")
-      return meanByDay
+      console.log('dataByDayYear: end', meanByDayOfYear)
+      return meanByDayOfYear
     }
   },
   mounted () {
-    this.getData()
+    // this.getData()
   },
   watch: {
     rawData () {
       console.log('watch:rawData', this.rawData)
       this.initializeCharts()
+    },
+    selectedFileName () {
+      this.getData()
     }
   },
   methods: {
@@ -133,8 +144,14 @@ console.log("filledData, end")
       this.brushExtent = extent
     },
     getData () {
-      const parseDate = d3.timeParse('%-m/%-d/%Y')
-      return d3.csv('data/1949884.csv', d => {
+      // for now
+      let parseDate = null
+      if (this.selectedFileName === '1949884.csv') {
+        parseDate = d3.timeParse('%-m/%-d/%Y')
+      } else if (this.selectedFileName === 'mitchellFromSHEDS.csv') {
+        parseDate = d3.timeParse('%-m/%-d/%Y %H:%M')
+      }
+      return d3.csv('data/' + this.selectedFileName, d => {
         d.date = parseDate(d.date)
         d.day = formatters.mdy(d.date)
         // d.dayOfYear = +formatters.julian(d.date)
@@ -153,7 +170,6 @@ console.log("filledData, end")
       // .on("brush", (brushExtent) => {
       //   this.$emit('brushed', brushExtent)
       // })
-      
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+// Vue.config.errorHandler = err => {
+//   console.log('Exception: ', err)
+// }
 
 new Vue({
   vuetify,
